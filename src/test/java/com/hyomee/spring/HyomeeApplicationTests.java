263afbc3eaef4aa35748c3a3302c86510215d6b4
hyosang <hyomee@naver.com>
2023-08-10 23:55:44 +0900

    Initial commit

---
+++ src/test/java/com/hyomee/spring/HyomeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyomee.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyomeeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
